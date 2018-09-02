337e4a206a28fb05e4fd64d64749b571a5ae8a33
Reza Shokri <bioharz3@gmail.com>
2018-09-02 22:49:39 +0700

    some basic dummy settings

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -7,6 +7,14 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+  settings = {
+    info_service: true,
+    push_service: false,
+    sw_mode: false,
+    dark_mode: false,
+    touch_id: false,
+  };
+
   constructor(public navCtrl: NavController) {
 
   }
